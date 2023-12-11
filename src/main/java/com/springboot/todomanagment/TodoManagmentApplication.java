d7b06d3c4d954741f65e4e89e14ee918dc7a79ee
JalesiaGriffin <jgriffin@appdev.yearup.org>
2023-12-11 18:32:46 -0500

    initial commit

---
+++ src/main/java/com/springboot/todomanagment/TodoManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.todomanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoManagmentApplication.class, args);
+	}
+
+}
